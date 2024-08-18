23eaf2dfe48bdc5a291edf8f14463d2b7c1b3ccc
chimex <chimex01.info@gmail.com>
2024-08-18 22:51:28 +0100

    Django Views and URL Configuration

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,5 @@
-from django.db import models 
+from django.db import models  
+from django.contrib.auth.models import User 
 
 class Author(models.Model):  
     name = models.CharField(max_length=100)
@@ -22,3 +23,28 @@ class Librarian(models.Model):
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
     def __str__(self):
         return self.name 
+
+
+ 
+
+class UserProfile(models.Model):  
+    USER_ROLES = [  
+        ('Admin', 'Admin'),  
+        ('Librarian', 'Librarian'),  
+        ('Member', 'Member'),  
+    ]  
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  
+    role = models.CharField(max_length=20, choices=USER_ROLES)  
+
+    def __str__(self):  
+        return f"{self.user.username} - {self.role}"
+    
+
+from django.db.models.signals import post_save  
+from django.dispatch import receiver  
+
+@receiver(post_save, sender=User)  
+def create_user_profile(sender, instance, created, **kwargs):  
+    if created:  
+        UserProfile.objects.create(user=instance)
