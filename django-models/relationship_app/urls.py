a6f2c4410896fa89f00e8c45a369e5ca4132cdcd
chimex <chimex01.info@gmail.com>
2024-08-18 22:12:38 +0100

    Django Views and URL Configuration

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path  
+from django.contrib.auth import views as auth_views 
 from . import views
-from .views import list_books  
+from .views import list_books 
+from .views import register, login_view, CustomLogoutView  
 
 urlpatterns = [  
     path('books/', list_books, name='list_books'),  
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  
+    path('register/', register, name='register'),  
+    path('login/', login_view, name='login'),  
+    path('logout/', CustomLogoutView.as_view(), name='logout'), 
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,13 +3,44 @@ from .models import Book
 from django.views.generic.detail import DetailView 
 from .models import Library 
 
+from django.contrib.auth import login, authenticate  
+from django.contrib.auth.forms import UserCreationForm  
+from django.shortcuts import render, redirect  
+from django.contrib.auth.views import LogoutView 
+
 def list_books(request):  
     books = Book.objects.all()  
     context = {'books': books}  
     return render(request, 'relationship_app/list_books.html', context)
 
-  
-
 class LibraryDetailView(DetailView):  
     model = Library  
     template_name = 'relationship_app/library_detail.html'
+
+
+# Registration View  
+def register(request):  
+    if request.method == 'POST':  
+        form = UserCreationForm(request.POST)  
+        if form.is_valid():  
+            user = form.save()  
+            login(request, user)  # Log the user in right after registration  
+            return redirect('list_books')  # Redirect to a page after registration  
+    else:  
+        form = UserCreationForm()  
+    return render(request, 'register.html', {'form': form})  
+
+# Login View  
+def login_view(request):  
+    if request.method == 'POST':  
+        username = request.POST['username']  
+        password = request.POST['password']  
+        user = authenticate(request, username=username, password=password)  
+        if user is not None:  
+            login(request, user)  
+            return redirect('list_books')  # Redirect to a page after login  
+    return render(request, 'login.html')  
+
+# Logout View (using built-in LogoutView)  
+class CustomLogoutView(LogoutView):  
+    template_name = 'logout.html' 
