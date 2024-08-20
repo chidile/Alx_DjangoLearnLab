c406a7a1850d43d1236b45c0b9b0cfa01d63daca
chimex01 <chimex01.info@gmail.com>
2024-08-20 10:32:15 +0100

    Implementing Custom Permissions in Django

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,6 +7,8 @@ from .views import LoginView, LogoutView
 
 from .views import admin_view, librarian_view, member_view 
 
+from .views import add_book, edit_book, delete_book
+
 urlpatterns = [  
     path('books/', list_books, name='list_books'),  
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'), 
@@ -18,4 +20,8 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),  
     path('librarian/', librarian_view, name='librarian_view'),  
     path('member/', member_view, name='member_view'),
+
+    path('books/add/', add_book, name='add_book'),  
+    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),  
+    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -64,3 +64,22 @@ def librarian_view(request):
 @user_passes_test(is_member)  
 def member_view(request):  
     return render(request, 'member_view.html')
+
+from django.contrib.auth.decorators import permission_required  
+
+@permission_required('relationship_app.can_add_book')  
+def add_book(request):  
+    # Logic to add book...  
+    pass
+
+@permission_required('relationship_app.can_change_book')  
+def edit_book(request, book_id):  
+    # Logic to edit book...  
+    pass
+
+@permission_required('relationship_app.can_delete_book')  
+def delete_book(request, book_id):  
+    # Logic to delete book...
+    pass
+
+    
