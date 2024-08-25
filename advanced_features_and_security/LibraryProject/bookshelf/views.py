3ff384728dc9ba7b3af4f05893aaf98a8f6e5697
chimex <chimex01.info@gmail.com>
2024-08-25 19:56:19 +0100

    Implementing corrections

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import permission_required 
- 
+from django.shortcuts import render, get_object_or_404  
+from django.contrib.auth.decorators import permission_required  
+from .models import   MyModel
 
-@permission_required('bookshelf.can_edit', raise_exception=True)  
+@permission_required('app_name.can_view', raise_exception=True)  
+def list_view(request):  
+    objects = MyModel.objects.all()  
+    return render(request, 'list.html', {'objects': objects})  
+
+@permission_required('app_name.can_create', raise_exception=True)  
+def create_view(request):  
+    if request.method == 'POST':  
+        # Logic to create a new MyModel instance  
+        pass  
+    return render(request, 'create.html')  
+
+@permission_required('app_name.can_edit', raise_exception=True)  
 def edit_view(request, pk):  
-    # View logic here
-    pass
+    obj = get_object_or_404(MyModel, pk=pk)  
+    if request.method == 'POST':  
+        # Logic to edit the MyModel instance  
+        pass  
+    return render(request, 'edit.html', {'object': obj})  
+
+@permission_required('app_name.can_delete', raise_exception=True)  
+def delete_view(request, pk):  
+    obj = get_object_or_404(MyModel, pk=pk)  
+    if request.method == 'POST':  
+        obj.delete()  
+        # Redirect after deletion  
+    return render(request, 'delete.html', {'object': obj})
