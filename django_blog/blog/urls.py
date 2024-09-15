b686c025ea2052f60a5b3d9b4aaaa5a26c0a04e0
chimex <chimex01.info@gmail.com>
2024-09-15 18:10:21 +0100

    Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 
     path('posts/<int:post_id>/comments/', CommentListView.as_view(), name='comment_list'),  
-    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment_create'),  
+    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment_create'),  
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_edit'),  
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),  
 ]
