dcfd2d25b54ebeceba1ac563026fbbce426f26f9
chimex <chimex01.info@gmail.com>
2024-09-15 17:06:30 +0100

    Implementing the Blog's User Authentication System

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('profile/edit/', views.edit_profile, name='edit_profile'),
 
     path('posts/', PostListView.as_view(), name='post_list'),  
-    path('posts/new/', PostCreateView.as_view(), name='post_create'),  
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),  
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),  
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),  
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),  
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_edit'),  
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
