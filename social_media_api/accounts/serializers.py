57264a46155c1ca933879de8c36e9a9e5799e27f
chimex <chimex01.info@gmail.com>
2024-09-22 12:32:11 +0100

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,15 +4,28 @@ from rest_framework.authtoken.models import Token
 
 User = get_user_model()  
 
+# Create a new user  
+user = User.objects.create_user(  
+    username='john_doe',  
+    email='john@example.com',  
+    password='secure_password123'  
+)  
+user.save()
+
+
 class UserSerializer(serializers.ModelSerializer):  
     class Meta:  
         model = User  
-        fields = ['id', 'username', 'bio', 'profile_picture', 'followers']  
+        fields = ['id', 'username', 'bio', 'profile_picture', 'followers'] 
+
+    def create(self, validated_data):
+        return super().create(validated_data) 
 
 class RegisterSerializer(serializers.ModelSerializer):  
     class Meta:  
         model = User  
-        fields = ['username', 'password', 'bio', 'profile_picture']  
+        fields = ['username', 'password', 'bio', 'profile_picture'] 
+        name = serializers.CharField(max_length=100, required=True)   
 
     def create(self, validated_data):  
         user = User(**validated_data)  
